58511d263d099290618046bd799401f8069e5e3d
demitra <hellodemitra@gmail.com>
2023-07-09 17:22:36 +0300

    create projectiles

--- index.js
+++ index.js
@@ -59,7 +59,31 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor({position, velocity}) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.radius = 3;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = 'red';
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 const player = new Player();
+const projectiles = [];
 const keys = {
   a: {
     pressed: false,
@@ -69,7 +93,7 @@ const keys = {
   },
   space: {
     pressed: false,
-  },
+  }
 };
 
 function animate() {
@@ -77,10 +101,19 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    } else {
+      projectile.update();
+    }
+  });
 
   if (keys.a.pressed && player.position.x >= 0) {
     player.velocity.x = -7;
-    player.rotation = -.15;
+    player.rotation = -0.15;
   } else if (
     keys.d.pressed &&
     player.position.x + player.width <= canvas.width
@@ -98,15 +131,28 @@ animate();
 window.addEventListener("keydown", ({ key }) => {
   switch (key) {
     case "a":
-      console.log("left");
+      // console.log("left");
       keys.a.pressed = true;
       break;
     case "d":
-      console.log("right");
+      // console.log("right");
       keys.d.pressed = true;
       break;
     case " ":
-      console.log("space");
+      // console.log("space");
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 2,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+      console.log(projectiles);
       break;
   }
 });
